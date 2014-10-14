d14f90e442f50087e456020a8030bbfdf0123286
Alex Petrov <alexp@coffeenco.de>
2014-10-14 23:35:52 +0200

    Start working on config files

--- src/write_nanomsg.c
+++ src/write_nanomsg.c
@@ -6,13 +6,13 @@
 #include "plugin.h"
 
 /* Ethernet - (IPv6 + TCP) = 1500 - (40 + 32) = 1428 */
-#ifndef WG_SEND_BUF_SIZE
-# define WG_SEND_BUF_SIZE 1428
+#ifndef NMSG_SEND_BUF_SIZE
+# define NMSG_SEND_BUF_SIZE 1428
 #endif
 
 struct nanomsg_callback {
   char *connect_uri;
-  char send_buf[]
+  char send_buf[NMSG_SEND_BUF_SIZE];
   int  nanomsg_sock;
 };
 
@@ -24,26 +24,48 @@ static int nanomsg_write(const data_set_t *ds, /* {{{ */
   return 1;
 }
 
-static int nanomsg_config_node (oconfig_item_t *node_config) {
-  // ..
+static int nanomsg_config_node (oconfig_item_t *cfg) {
+  struct nanomsg_callback *node_config;
+  node_config = malloc(sizeof(*node_config));
+
+  if (node_config) {
+    ERROR("MALLOC FAILED");
+    return -1;
+  }
+
+  int i;
+  for (i = 0; i < cfg->children_num; i++) {
+    oconfig_item_t *child = &cfg->children[i];
+
+    if (strcasecmp ("uri", child->key))
+      cf_util_get_string(child, &node_config->connect_uri);
+  }
+  return 0;
 }
 
 static int nanomsg_config (oconfig_item_t *ci)
 {
   int i;
   for (i = 0; i < ci->children_num; i++) {
-
+    oconfig_item_t *child = &ci->children[i];
+    if (strcasecmp ("Server", child->key)) {
+      nanomsg_config_node(child);
+    } else {
+      ERROR("Invalid configuration option: %s", child->key);
+    }
   }
+
+  return 0;
 }
 
-static int nanomsg_shutdown (void)
-{
+/* static int nanomsg_shutdown (void) */
+/* { */
 
-}
+/* } */
 
 
 void module_register (void)
 {
   plugin_register_complex_config ("write_nanomsg", nanomsg_config);
-  plugin_register_shutdown ("write_nanomsg", nanomsg_shutdown);
+  /* plugin_register_shutdown ("write_nanomsg", nanomsg_shutdown); */
 }
